ce7b98c20920620feadc309f32f0b5e81dc9680e
asinugobi <asinugo@seas.upenn.edu>
2018-04-15 15:17:07 -0400

    implemented simple cnn model

--- sequential/models/simple_cnn_model.py
+++ sequential/models/simple_cnn_model.py
@@ -1,10 +1,10 @@
 from base.base_model import BaseModel
-from networks.ffn import FeedForwardNet
+from networks.simple_cnn import SimpleCNN
 import tensorflow as tf
 
-class FFNModel(BaseModel):
+class SimpleCNNModel(BaseModel):
     def __init__(self, config):
-        super(FFNModel, self).__init__(config)
+        super(SimpleCNNModel, self).__init__(config)
         self.build_model()
         self.init_saver()
 
@@ -14,10 +14,17 @@ class FFNModel(BaseModel):
         # define placeholder for x and y 
         self.x = tf.placeholder(tf.float32, shape=[None, 784])
         self.y = tf.placeholder(tf.float32, shape=[None, 10])
+        self.phase = tf.placeholder(tf.bool, name='phase')
+        self.dropout = tf.placeholder(dtype=tf.float32, name='dropout')
 
-        # network architecture (simple 2-layer ffn)
-        self.net = FeedForwardNet(self.x, self.y)
-        self.net.create_graph() 
+        # network architecture (simple 4-layer cnn)
+        self.cnn = SimpleCNN(input=self.x,
+                        phase=self.phase,
+                        dropout=self.dropout,
+                        num_classes=self.config.num_classes,
+                        skip_layer=[''],
+                        weights_path='DEFAULT')
+        self.cnn.create_graph() 
 
         # define loss 
         self.loss() 
@@ -31,20 +38,20 @@ class FFNModel(BaseModel):
     def loss(self): 
         # loss definition 
         with tf.name_scope('loss'):
-            self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.net.y))
+            self.cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.cnn.sigma))
 
     def optimize(self):
         # NOTE: when training, the moving_mean and moving_variance need to be updated. By default the update ops are placed in tf.GraphKeys.UPDATE_OPS, so they need to be added as a dependency to the train_op.(https://www.tensorflow.org/api_docs/python/tf/layers/batch_normalization)
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         
         with tf.control_dependencies(update_ops), tf.name_scope('optimize'): 
-            learning_rate = 0.1 
+            learning_rate = self.config.learning_rate
             optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
             self.train_step = optimizer.minimize(self.cross_entropy)
 
     def set_metrics(self): 
         with tf.name_scope('accuracy'): 
-                    correct_prediction = tf.equal(tf.argmax(self.net.y, 1), tf.argmax(self.y, 1))
+                    correct_prediction = tf.equal(tf.argmax(self.cnn.sigma, 1), tf.argmax(self.y, 1))
                     self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     def init_saver(self):
