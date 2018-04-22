e12fde0d93c30836c220125efbb6abf74b379985
asinugobi <asinugo@seas.upenn.edu>
2018-04-22 08:38:24 -0400

    implemented penalty vs loss functionality

--- sequential/mains/ffn_varying_ewc.py
+++ sequential/mains/ffn_varying_ewc.py
@@ -9,7 +9,7 @@ from data_loader.data_handler import DataHandler
 from models.ffn_model import FFNModel
 from trainers.ffn_trainer import FFNTrainer
 from utils.config import process_config
-from utils.plotting import plot_results
+from utils.plotting import *
 from utils.dirs import create_dirs
 from utils.logger import Logger
 from utils.utils import get_args
@@ -85,18 +85,23 @@ def main():
     min = 0
     max = 201
     step = 20
-    ewc_penalty = range(min, max, step)
+    # ewc_penalty = range(min, max, step)
+    ewc_penalty = [0, 15]
 
-    for lambda in ewc_penalty: 
+    # reset paramaters for training on new data 
+    permutated_mnist_2 = data.permute_mnist() 
+    average_losses = [] 
+    
+    for penalty in ewc_penalty: 
+        # restore original model 
+        model.load(sess)
+
         # set loss/optimizer 
-        model.set_ewc_loss(lam=lambda)
+        model.set_ewc_loss(lam=penalty)
         model.reset_train_step(loss=model.ewc_loss, variables=variables)
 
-        # reset paramaters for training on new data 
-        permutated_mnist_2 = data.permute_mnist() 
-        trainer.reset(permutated_mnist_2)
-
         # train on new dataset 
+        trainer.reset(permutated_mnist_2)
         trainer.train()
         
         # plot results 
@@ -105,7 +110,18 @@ def main():
             test_plots[0].append(trainer.all_test_accuracies[idx][0])
             test_plots[1].append(trainer.all_test_accuracies[idx][1])
 
-        plot_results(num_iterations=config.num_epochs+1, train_plots=trainer.train_accuracy, test_plots=test_plots)
+        loss_plots = [[] for x in range(2)]
+        for idx in range(config.num_epochs+1):
+            loss_plots[0].append(trainer.all_test_losses[idx][0])
+            loss_plots[1].append(trainer.all_test_losses[idx][1])
+
+        average_losses.append(np.mean(loss_plots[0]))
+
+        # plot_results(num_iterations=config.num_epochs+1, train_plots=trainer.train_accuracy, test_plots=test_plots, loss_plots=loss_plots, save=True, show=False, path='../figures/', experiment='testing' + str(penalty))
+
+    plot_varying_penalty(penalties=ewc_penalty, average_loss=average_losses)
+
+         
 
 if __name__ == '__main__':
     main()
--- sequential/utils/plotting.py
+++ sequential/utils/plotting.py
@@ -2,28 +2,50 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 # classification accuracy plotting
-def plot_results(num_iterations=100, train_plots=[], test_plots=[], loss_plots=[]):
+def plot_results(num_iterations=100, train_plots=[], test_plots=[], loss_plots=[], save=False, show=True, path='', experiment=''):
     colors = ['r', 'b', 'g']
 
     # plot training results 
     iterations = range(num_iterations)
 
-    train = plt.figure(1)
+    train = plt.figure()
     plt.plot(iterations, train_plots, 'r')
     plt.xlabel('Iterations')
     plt.ylabel('Train Accuracy')
 
-    test = plt.figure(2)
+    test = plt.figure()
     for idx in range(len(test_plots)): 
         plt.plot(iterations, test_plots[idx], colors[idx])
     plt.xlabel('Iterations')
     plt.ylabel('Test Accuracy')
 
-    loss = plt.figure(3)
+    loss = plt.figure()
     for idx in range(len(loss_plots)): 
         plt.plot(iterations, loss_plots[idx], colors[idx])
     plt.xlabel('Iterations')
     plt.ylabel('Average Loss')
 
+    if(save):
+        train.savefig(path + experiment + '_train.png')
+        test.savefig(path + experiment + '_test.png')
+        loss.savefig(path + experiment + '_loss.png')
+
+    if(show):
+        plt.show() 
+
+    train.clf()
+    test.clf()
+    loss.clf()
+
+def plot_varying_penalty(penalties=[], average_loss=[]):
+    # plot varying loss vs. penalties results 
+    loss = plt.figure() 
+    plt.plot(penalties, average_loss, 'r')
+    plt.xlabel('EWC Penalties')
+    plt.ylabel('Average Loss')
     plt.show() 
+
+
+
+    
 
