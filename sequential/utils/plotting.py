136c79f37cf0106f3a1765b2eae158dd63a0be91
asinugobi <asinugo@seas.upenn.edu>
2018-05-06 22:13:41 -0400

    added legend to plotting

--- sequential/utils/plotting.py
+++ sequential/utils/plotting.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 # classification accuracy plotting
-def plot_results(num_iterations=100, train_plots=[], test_plots=[], loss_plots=[], save=False, show=True, path='', experiment=''):
+def plot_results(num_iterations=100, train_plots=[], test_plots=[], loss_plots=[], save=False, show=True, path='', experiment='', title=''):
     colors = ['r', 'b', 'g']
+    labels = ['Source', 'Target', 'Target 2'] 
 
     # plot training results 
     iterations = range(num_iterations)
@@ -12,18 +13,23 @@ def plot_results(num_iterations=100, train_plots=[], test_plots=[], loss_plots=[
     plt.plot(iterations, train_plots, 'r')
     plt.xlabel('Iterations')
     plt.ylabel('Train Accuracy')
+    plt.title(title + ': Train Accuracy vs. Iterations')
 
     test = plt.figure()
     for idx in range(len(test_plots)): 
-        plt.plot(iterations, test_plots[idx], colors[idx])
+        plt.plot(iterations, test_plots[idx], colors[idx], label=labels[idx])
     plt.xlabel('Iterations')
     plt.ylabel('Test Accuracy')
+    plt.title(title + ': Test Accuracy vs. Iterations')
+    plt.legend(labels[:len(test_plots)])
 
     loss = plt.figure()
     for idx in range(len(loss_plots)): 
-        plt.plot(iterations, loss_plots[idx], colors[idx])
+        plt.plot(iterations, loss_plots[idx], colors[idx], label=labels[idx])
     plt.xlabel('Iterations')
     plt.ylabel('Average Loss')
+    plt.title(title + ': Average Loss vs. Iterations')
+    plt.legend(labels[:len(loss_plots)])
 
     if(save):
         train.savefig(path + experiment + '_train.png')
@@ -41,9 +47,10 @@ def plot_varying_penalty(penalties=[], average_loss=[], path='', experiment='', 
     # plot varying loss vs. penalties results 
     loss = plt.figure() 
     plt.plot(penalties, average_loss, 'r.')
-    plt.xlabel('EWC Penalties')
+    plt.xlabel('EWC Penalty')
     plt.ylabel('Average Loss')
-    
+    plt.title(title + ': Average Loss vs. EWC Penalty') 
+
     if (save): 
         loss.savefig(path + experiment + 'loss_vs_penalty.png')
     
