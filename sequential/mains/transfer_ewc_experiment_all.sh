19b3b9a6e50db56b07e5073db3124cbdae34127b
asinugobi <asinugo@seas.upenn.edu>
2018-05-09 11:50:09 -0400

    experimental results for 4/8

--- sequential/mains/transfer_ewc_experiment_all.sh
+++ sequential/mains/transfer_ewc_experiment_all.sh
@@ -2,20 +2,19 @@
 # Shell script for running transfer ewc experiments 
 
 # Experiment 1: Baseline (no ewc; transfer both conv layers)
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0.json
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0.json
 
 # Experiment 2: EWC on second conv layer only  
 python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_4.json
 
 # Experiment 3: EWC on both conv layers only 
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_8.json
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_8.json
 
 # Experiment 4: EWC on second conv layer + fully-connected layers 
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_4b.json
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_4b.json
 
 # Experiment 5: EWC on both conv layers + fully-connected layers 
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_8b.json
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_8b.json
 
 # Experiment 6: EWC on fully-connected layers only 
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0b.json
- 
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0b.json
