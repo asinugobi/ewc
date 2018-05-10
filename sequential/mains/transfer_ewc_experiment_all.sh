c0af9b770af366517ff03492206a70d441b824eb
asinugobi <asinugo@seas.upenn.edu>
2018-05-10 10:08:19 -0400

    converted images from jpg to png

--- sequential/mains/transfer_ewc_experiment_all.sh
+++ sequential/mains/transfer_ewc_experiment_all.sh
@@ -2,10 +2,10 @@
 # Shell script for running transfer ewc experiments 
 
 # Experiment 1: Baseline (no ewc; transfer both conv layers)
-# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0.json
+python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_0.json
 
 # Experiment 2: EWC on second conv layer only  
-python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_4.json
+# python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_4.json
 
 # Experiment 3: EWC on both conv layers only 
 # python simple_cnn_layerwise_ewc.py -c ../configs/simple_cnn_layerwise_8.json
